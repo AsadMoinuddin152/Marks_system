20f09e7463f5454a7c701e3256690ad0da6483c0
wireframeee <brightly2469@gmail.com>
2023-11-24 19:02:48 +0530

    linked css

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Form, Input } from "antd";
 import { useNavigate } from "react-router-dom";
 import '../styles/LoginPage.css'
+import user from '../user.png';
+import lock from '../padlock.png';
 
 const Login = () => {
   const navigate = useNavigate();
@@ -10,34 +12,39 @@ const Login = () => {
     navigate("/homepage");
   };
   return (
-    <div>
-      <Form
-        name="basic"
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-      >
-        Login
-        <Form.Item
-          label="Email"
-          name="email"
-          rules={[{ required: true, message: "Please input your Email!" }]}
+    <div className="login-page">
+      <div className="login-back">
+        <div className="login-text">LOGIN</div>
+        <Form
+          className='login-form'
+          name="basic"
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
         >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label="Password"
-          name="password"
-          rules={[
-            { required: true, message: "Please input your Password!" },
-            { min: 6, message: "Password must be minimum 6 characters." },
-          ]}
-        >
-          <Input.Password />
-        </Form.Item>
-        <Form.Item wrapperCol={{ offset: 8, span: 8 }}>
-          <button type="submit">Submit</button>
-        </Form.Item>
-      </Form>
+          <div className="inputbox">
+            <img className='logo' src={user} alt="user"/>
+            <div
+              name="email"
+              rules={[{ required: true, message: "Please input your Email!" }]}
+            >
+              <input className="textbox" placeholder="enter username"/>
+            </div>
+          </div>
+          <div className="inputbox">
+            <img className='logo' src={lock} alt="lock"/>
+            <div
+              name="password"
+              rules={[
+                { required: true, message: "Please input your Password!" },
+                { min: 6, message: "Password must be minimum 6 characters." },
+              ]}
+            >
+              <input type="password" className="textbox" placeholder="enter password"/>
+            </div>
+          </div>
+            <button type="submit" className="login-button">SUBMIT</button>
+        </Form>
+      </div>
     </div>
   );
 };
