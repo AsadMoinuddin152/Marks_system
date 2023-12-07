bb5b940e74d6f070c200ed04f3a89163727b7fd2
wireframeee <brightly2469@gmail.com>
2023-12-07 08:11:28 +0530

    working on report generation

--- client/src/Pages/Report.js
+++ client/src/Pages/Report.js
@@ -1,40 +1,88 @@
-import React from "react";
-import { Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer';
-
-const styles = StyleSheet.create({
-    page: {
-      flexDirection: 'row',
-      backgroundColor: '#2f76ff',
-      size: "A4",
-    },
-    section: {
-      margin: 100,
-      padding: 100,
-      flexGrow: 1
-    }
-  });
+import React from 'react';
+import Data from '../temp/data.json';
+import { PDFDocument } from 'pdf-lib';
 
 const Report = () => {
+    const [file, setFile] = useState(null);
+   
+    React.useEffect(() => {
+       writeDataToPDF();
+    }, []);
 
-    const MyDocument = () => (
-    <Document>
-        <Page style={styles.page}>
-        <View style={styles.section}>
-            <Text>Section #1</Text>
-        </View>
-        <View style={styles.section}>
-            <Text>Section #2</Text>
-        </View>
-        </Page>
-    </Document>
-    );
+    async function writeDataToPDF() {
+    // Load an existing PDF
+    const pdfDoc = await PDFDocument.load('/pdf.pdf');
+
+    // Embed the Helvetica font
+    const helveticaFont = await pdfDoc.embedFont(PDFDocument.Font.Helvetica);
+
+    // Get the first page of the PDF
+    const firstPage = pdfDoc.getPages()[0];
+
+    // Get the width and height of the first page
+    const { width, height } = firstPage.getSize();
+
+    // Add each student to the PDF
+    Data.forEach((student, index) => {
+        const yOffset = height - (20 * (index + 1));
+
+        // Draw the student ID on the first page
+        firstPage.drawText(`ID: ${student.id}`, {
+            x: 5,
+            y: yOffset,
+            size: 14,
+            font: helveticaFont,
+        });
+
+        // Draw the student name on the first page
+        firstPage.drawText(`Name: ${student.name}`, {
+            x: 5,
+            y: yOffset - 20,
+            size: 14,
+            font: helveticaFont,
+        });
+
+        // Draw the student department on the first page
+        firstPage.drawText(`Department: ${student.dept}`, {
+            x: 5,
+            y: yOffset - 40,
+            size: 14,
+            font: helveticaFont,
+        });
+    });
+
+    // Serialize the PDF to bytes (a Uint8Array)
+    const pdfBytes = await pdfDoc.save();
+
+     // Create a Blob from the bytes
+     const pdfBlob = new Blob([pdfBytes], { type: 'application/pdf' });
+
+     // Create a local object URL for the Blob
+     const pdfUrl = URL.createObjectURL(pdfBlob);
+ 
+     // Update the file state to trigger a re-render with the new PDF URL
+     setFile(pdfUrl);
+
+    // For example, you could write these bytes to a file
+    // or send them to a server, etc.
+}
+    React.useEffect(() => {
+        writeDataToPDF();
+    }, []);
 
     return (
-        <>
-            <h1>Report</h1>
-            <MyDocument />
-        </>
-    );
-};
+        <div>
+            {
+                Data.map(student => (
+                    <tr key={student.id}>
+                        <td>{student.id}</td>
+                        <td>{student.name}</td>
+                        <td>Department: {student.dept}</td>
+                    </tr>
+                ))
+            }
+        </div>
+    )
+}
 
 export default Report
